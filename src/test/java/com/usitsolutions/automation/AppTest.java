bef7cf30912bef9a2629f1e2cc4fece18a0044f0
Uma <usachary@gmail.com>
2015-08-21 19:21:32 +0100

    initialupload

---
+++ src/test/java/com/usitsolutions/automation/AppTest.java
@@ -0,0 +1,12 @@
+package com.usitsolutions.automation;
+
+import org.testng.annotations.Test;
+
+public class AppTest{
+
+    @Test
+    public void sendMail(){
+        System.out.println("****************App Test*************");
+    }
+
+}
---
+++ src/test/java/com/usitsolutions/automation/SampleTest.java
@@ -0,0 +1,14 @@
+package com.usitsolutions.automation;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Revathi on 21/08/2015.
+ */
+public class SampleTest {
+
+    @Test
+    public void sampleTest(){
+        System.out.println("****************Sample Test*************");
+    }
+}
